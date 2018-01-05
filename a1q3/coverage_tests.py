0af7e8d05f22b9e963d869d6afdc84d0571c501f
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2018-01-05 14:25:03 -0500

    Initial import of a1q3

---
+++ a1q3/coverage_tests.py
@@ -0,0 +1,21 @@
+import unittest
+from a1q3 import M
+
+class CoverageTests (unittest.TestCase):
+    def test_1 (self):
+        """Comment"""
+        o = M ()
+        pass
+    def test_2 (self):
+        """Comment"""
+        o = M ()
+        pass
+    def test_3 (self):
+        """Comment"""
+        o = M ()
+        pass
+    def test_4 (self):
+        """Comment"""
+        o = M ()
+        pass
+    
