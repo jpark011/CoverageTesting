7bfb2aa6e55c8e313c1dcdba178d730b91ab03a3
Jay Park <wngt3319@gmail.com>
2018-02-05 00:07:06 -0500

    A03c done

--- a1q3/coverage_tests.py
+++ a1q3/coverage_tests.py
@@ -5,17 +5,37 @@ class CoverageTests (unittest.TestCase):
     def test_1 (self):
         """Comment"""
         o = M ()
+        # NC
+        o.m([], 0)
+        o.m([1], 0)
+        o.m([1,2], 0)
+        o.m([1,2,3], 0)
         pass
+
     def test_2 (self):
         """Comment"""
         o = M ()
+        # NC
+        o.m([], 0)
+        o.m([1], 0)
+        o.m([1,2], 0)
+        o.m([1,2,3], 0)
+        # need 1 more case for EC
+        o.m([], 1)
         pass
+
     def test_3 (self):
         """Comment"""
         o = M ()
+        # EPC does not differ from EC
+        # since infeasible edge pairs exist
+        # those feasible are covered by EC
         pass
+
     def test_4 (self):
         """Comment"""
         o = M ()
+        # Similar to EPC, PPC does not differ from EC
+        # since infeasible prime paths exist
+        # and those feasilbe are already covered by EC
         pass
-    
